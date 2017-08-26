443ec5dda7b35e79510a94405cba9c8afcd068e6
happyKen <2815506514@qq.com>
2017-08-26 11:06:22 +0800

    sharepreferences

---
+++ MyUtil/app/src/main/java/com/common/myutil/sharepreferences/VoiceAutoPlayPreferences.java
@@ -0,0 +1,18 @@
+package com.common.myutil.sharepreferences;
+
+
+import com.common.myutil.base.BaseSharePreferences;
+
+/**
+ * Created by wuguohui on 2017/3/7.
+ */
+
+public class VoiceAutoPlayPreferences extends BaseSharePreferences {
+
+    public static final String Key_AutoPlay = "autoplay";
+
+    static {
+        fileName = "voice";
+    }
+
+}
