d4716f0c6a27d05a67d8abeab4a9d3317d3445e0
happyKen <2815506514@qq.com>
2017-08-26 14:45:24 +0800

    densityUtils

--- MyUtil/app/src/main/java/com/common/myutil/utils/DensityUtils.java
+++ MyUtil/app/src/main/java/com/common/myutil/utils/DensityUtils.java
@@ -9,9 +9,53 @@ import android.view.View;
  */
 
 public class DensityUtils {
-    public static int dp2px(Context context, float dp) {
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     *
+     * @param context 上下文
+     * @param dpValue 值
+     * @return 转换结果
+     */
+    public static int dp2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     *
+     * @param context 上下文
+     * @param pxValue 值
+     * @return 转换结果
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param context 上下文
+     * @param spValue 值
+     * @return 转换结果
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param context 上下文
+     * @param pxValue 值
+     * @return 转换结果
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
     }
 
 
