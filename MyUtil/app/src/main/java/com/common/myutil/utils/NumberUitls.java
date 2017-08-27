9164e7c03cc507f22ef8cf586d45c8f33a0567f8
happyKen <2815506514@qq.com>
2017-08-27 10:57:47 +0800

    number

---
+++ MyUtil/app/src/main/java/com/common/myutil/utils/NumberUitls.java
@@ -0,0 +1,26 @@
+package com.common.myutil.utils;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by wangzekeng on 2017/6/21.
+ */
+
+public class NumberUitls {
+
+    /**
+     * 转化成以万为单位
+     *
+     * @param num
+     * @return
+     */
+    public static String formatWan(long num) {
+        String s = num + "";
+        if (num > 10000) {
+            DecimalFormat df = new DecimalFormat("###.00");
+            float f = num / 10000.0f;
+            s = df.format(f) + "万";
+        }
+        return s;
+    }
+}
